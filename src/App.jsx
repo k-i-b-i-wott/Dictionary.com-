96d22dfa8cb5c97815d4b2ba914e244a3d907190
Kibiwott <bk.kibiwott@gmail.com>
2025-03-16 14:31:26 +0300

    Final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,67 @@
 import React from 'react'
  import './App.css'
-import{ useState} from 'react'
+import{ useState, useEffect} from 'react'
 const App = () => {
 
   const [word, setWord] = useState('');  
   const [definition, setDefinition] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const[error, setError] = useState(null);
+
+
 
   async function dictionaryApi() {
     try {
+      setLoading(true);
+      setError(null);
       const response = await fetch(
         `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
       );
+      if (!response.ok) {
+        throw new Error('There was an error finding the word');
+      }
       const data= await response.json();    
        
       console.log(data);
       setDefinition(data);
+      
     } catch (error) {
-      console.error(error);
+      setDefinition(null);
+      setError(error.message);
     }
-  };
+    finally {
+      setLoading(false);
+    }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    dictionaryApi();
+  }
+useEffect(() => {
+  if (word.trim() === '') {
+    setDefinition(null);
+    setError(null);
+    setLoading(false);
+    return;
+  }
+  dictionaryApi();
+}, [word]);
 
-  };
+const handleSubmit = (e) => {
+  e.preventDefault();
+
+  dictionaryApi();
+}
+
   return (
     <div >
       <Details
       word={word}
       setWord={setWord}
-      definition={setDefinition}
+      definition={definition}
       handleSubmit={handleSubmit}
 
       />
       <div className="output">
+        {loading && <div>Loading...Please wait</div>}
+        {error && <div>{error}</div>}
         {definition && (
           <div>
             <h3>{definition[0].word}</h3>
@@ -50,19 +77,13 @@ const App = () => {
                 </ul>
               </div>
             ))}
-          </div>
-
-        
-  
+          </div> 
         )}
-
-
-       
-      </div>
+        </div>
     </div>
   )
-}
 
+}
 export default App
 
 function Details({word, setWord, definition, handleSubmit}) {
@@ -74,7 +95,7 @@ function Details({word, setWord, definition, handleSubmit}) {
       <div>
         <h1>Search For any word</h1>
       </div>
-      <form action="">
+      <form >
         <input type="text" value={word} onChange={(e) => setWord(e.target.value)}  />
         <button type="submit" onClick={handleSubmit}>Search</button>
       </form>
